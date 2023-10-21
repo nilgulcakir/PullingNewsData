241aaeecf72fe2c13e8a285d6169db5e9ec69d1e
Nilgül Çakır <75910659+nilgulcakir@users.noreply.github.com>
2023-10-21 20:06:09 +0300

    Initial Commit

---
+++ PullingNewsData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PullingNewsData
+//
+//  Created by Nilgul Cakir on 21.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
